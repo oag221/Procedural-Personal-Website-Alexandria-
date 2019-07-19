5292e8208f88869422f7ceea76696c3b888dcc60
William Peracchio <willperacchio@users.noreply.github.com>
2019-07-18 23:25:12 -0400

    Complete refactor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
+import jquery from 'jquery';
+require('bootstrap')
+require('jquery.easing')
+
+require('bootstrap/dist/css/bootstrap.css')
+require('devicons/css/devicons.css')
+require('@fortawesome/fontawesome-free/css/all.min.css')
+require('./assets/css/resume.min.css');
+
+Vue.prototype.jquery = jquery;
 
 Vue.config.productionTip = false;
 
